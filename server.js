a6422deba2209d4fd179a013cf8a233f3bff3e68
martaluiz <martaluizavelino@gmail.com>
2020-04-13 12:34:02 -0700

    created a volunteer route

--- server.js
+++ server.js
@@ -34,10 +34,12 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
+const volunteersRoutes = require("./routes/volunteers");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
+app.use("/api/volunteers", volunteersRoutes(db));
 
 // Note: mount other resources here, using the same pattern above
 
