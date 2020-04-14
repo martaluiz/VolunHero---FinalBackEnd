c2d5fd354b0544c2f123bbe186328d90a13c6db5
Jasmine <jasmine_singh1992@hotmail.com>
2020-04-14 12:09:05 -0700

    added users routes, and made changes to database

--- routes/users.js
+++ routes/users.js
@@ -19,5 +19,45 @@ module.exports = (db) => {
         res.status(500).json({ error: err.message });
       });
   });
+
+  router.get("/:userId", (req, res) => {
+    db.query(
+      `select *
+     from users
+     where users.id = ${req.params.userId}`
+    )
+      .then((data) => {
+        const users = data.rows;
+        res.json({ users });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  ///POST == create a new user/// this works!
+  router.post("/create", (req, res) => {
+    let name = req.body.name;
+    let email = req.body.email;
+    let password = req.body.password;
+    let phone = req.body.phone;
+    let address = req.body.address;
+    let city = req.body.city;
+    let province = req.body.province;
+    let country = req.body.country;
+    let postalcode = req.body.postalcode;
+    db.query(
+      `INSERT INTO users (name, email, password, phone, address, city, province, country, postalcode)
+      VALUES ( '${name}', '${email}', '${password}', '${phone}', '${address}', '${city}', '${province}', '${country}', '${postalcode}') RETURNING * `
+    )
+      .then((data) => {
+        const new_user = data.rows[0];
+        res.json({ new_user });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
   return router;
 };
