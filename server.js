5edc1984ef0d1e0396125066e6d1b0382b2266fc
Jasmine <jasmine_singh1992@hotmail.com>
2020-04-14 23:17:03 +0000

    added cors

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 const sass = require("node-sass-middleware");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 // PG database client/connection setup
 const { Pool } = require("pg");
@@ -20,7 +21,7 @@ db.connect();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
-
+app.use(cors());
 // app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
@@ -57,6 +58,10 @@ app.use("/api/categories", categoriesRoutes(db));
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/with-cors", cors(), (req, res, next) => {
+  res.json({ msg: "WHOAH with CORS it works! 🔝 🎉" });
 });
 
 app
