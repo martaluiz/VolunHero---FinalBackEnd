9d9753763efccfdefe595dcf8c40400f9c0ac80b
Jasmine <jasmine_singh1992@hotmail.com>
2020-05-01 21:06:34 +0000

    cleaning up unused code

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,8 +1,6 @@
 const pg = require("pg");
 
 const pool = new pg.Pool({
-  // connectionString: process.env.DATABASE_URL || ""
-
   user: "final",
   password: "final",
   host: "localhost",
@@ -14,13 +12,3 @@ pool
   .catch((e) => console.log(`Error connecting to Postgres server:\n${e}`));
 
 module.exports = pool;
-
-// const { Pool } = require(`pg`);
-// const pool = new Pool({
-//   user: 'final',
-//   password: 'final',
-//   host: 'localhost',
-//   database: 'final',
-// });
-
-// module.exports = pool;
